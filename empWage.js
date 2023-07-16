6e292f22475ef92dac72537d3c1987a37fe5b3ee
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-16 17:34:39 +0530

    [Ritvik]Add: Using Function to calculate EmployeeWage

--- empWage.js
+++ empWage.js
@@ -1,34 +1,25 @@
 console.log("Welcome To Employee Wage Problem in Java Script!!!\n");
-
-// UC1 - Employee is Present or Absent
-{
-    const IS_ABSENT = 0;
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if(empCheck == IS_ABSENT){
-    console.log("Employee is Absent"); 
-    }else{
-    console.log("Employee is Present");
-    }
-}
-
+//UC3--Using Functions
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
-
 let empHrs =0;
-empCheck = Math.floor(Math.random() * 10) %3;
-switch(empCheck){
-    case IS_PART_TIME :
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME :
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs =0;
+
+function GetWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME :
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME :
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
 }
 
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = GetWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOURS;
 console.log("EmpWage is : "+empWage);
