6e6611bf904c731fadac2149eec31056fa08bd6b
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-16 17:46:01 +0530

    [Ritvik]Add: Using Repetitive Statements

--- empWage.js
+++ empWage.js
@@ -1,11 +1,12 @@
 console.log("Welcome To Employee Wage Problem in Java Script!!!\n");
-//UC3--Using Functions
+//UC3--Using Repititive Statements
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
-let empHrs =0;
+const NUMBER_OF_WORKING_DAYS = 20;
+let empCheck = 0;
 
 function GetWorkingHours(empCheck){
     switch(empCheck){
@@ -19,7 +20,10 @@ function GetWorkingHours(empCheck){
     }
 }
 
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = GetWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOURS;
-console.log("EmpWage is : "+empWage);
+let totalEmpHrs = 0;
+for(let day = 0;day < NUMBER_OF_WORKING_DAYS;day++){
+    let empCheck = Math.floor(Math.random() *10 ) %3;
+    totalEmpHrs +=GetWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HOURS;
+console.log("Employee Hours : "+totalEmpHrs+" EmpWage is : "+empWage);
