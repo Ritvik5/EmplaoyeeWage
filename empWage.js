b1e87b353f93987b83dddfcbe57807034f4c8ec2
Ritvik5 <sharma.ritvik56@gmail.com>
2023-07-16 15:33:02 +0530

    [Ritvik]Add; Checking Employee is Present or Absent

--- empWage.js
+++ empWage.js
@@ -1,1 +1,12 @@
-console.log("Welcome To Employee Wage Problem in Java Script!!!");
+console.log("Welcome To Employee Wage Problem in Java Script!!!\n");
+
+// UC1 - Employee is Present or Absent
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == IS_ABSENT){
+    console.log("Employee is Absent"); 
+    }else{
+    console.log("Employee is Present");
+    }
+}
